d438701d43dc6e43ffdc3d821b7366045ec94d5f
Jacob Hohing <jakehohing@yahoo.com>
2021-01-30 14:55:48 -0500

    Added Click and API functionality

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,22 @@
 
-var theURL = 0;
+var theKey = '&appid=7186d29486a92674ca72445a6647a5da';
+var theURL = 'https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=';
+
+$('#search-btn').click(function(){
+    
+    var theCity = $("#search-bar").val();
+    var queryURL = theURL + theCity + theKey
+    
+    $("#holder").html("");
+
+    console.log(queryURL)
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        contentType: "application/json",
+        dataType: "json",
+      }).then(function (Response) {
+            console.log(Response)
+      });
+    });
